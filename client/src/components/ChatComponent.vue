8fcfd4aff31d7b13aec3a9dcd18a74da77fb2e94
memoire <illusionsofmyworld@gmail.com>
2025-03-06 21:06:28 +0500

    Input field rework

--- client/src/components/ChatComponent.vue
+++ client/src/components/ChatComponent.vue
@@ -18,12 +18,41 @@
       <div v-if="isMiddleOpen" class="middle-panel-content">
         <!-- Header section -->
         <div class="chat-header" ref="chatHeader">
-          <div class="header-action" @click="toggleMiddlePanel">
-            <span class="close-mid-btn bi bi-list"></span>
-            <h3 :class="{ 'search-active': isSearchActive }">Чаты</h3>
+          <!-- Dropdown menu button -->
+          <div
+            class="plus-icon-wrapper"
+            :class="{ active: isMenuOpen }"
+            @click="toggleMenu"
+          >
+            <svg class="plus-icon" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+            </svg>
           </div>
 
-          <!-- Search Container -->
+          <!-- Chat title -->
+          <div class="header-action">
+            <h3
+              @click="toggleMiddlePanel"
+              :class="{ 'search-active': isSearchActive }"
+            >
+              Чаты
+            </h3>
+          </div>
+
+          <!-- Dropdown Menu -->
+          <div class="dropdown-menu-mid" :class="{ active: isMenuOpen }">
+            <div
+              class="menu-item"
+              v-for="(item, index) in menuItems"
+              :key="index"
+              @click.stop="handleMenuItemClick(item)"
+            >
+              <i :class="item.icon"></i>
+              <span>{{ item.label }}</span>
+            </div>
+          </div>
+
+          <!-- Search container -->
           <div class="search-container" :class="{ active: isSearchActive }">
             <input
               ref="searchInput"
@@ -42,29 +71,6 @@
                   d="M15.5 14H14.71L14.43 13.73C15.41 12.59 16 11.11 16 9.5C16 5.91 13.09 3 9.5 3C5.91 3 3 5.91 3 9.5C3 13.09 5.91 16 9.5 16C11.11 16 12.59 15.41 13.73 14.43L14 14.71V15.5L19 20.49L20.49 19L15.5 14ZM9.5 14C7.01 14 5 11.99 5 9.5C5 7.01 7.01 5 9.5 5C11.99 5 14 7.01 14 9.5C14 11.99 11.99 14 9.5 14Z"
                 />
               </svg>
-            </div>
-          </div>
-
-          <div
-            class="plus-icon-wrapper"
-            :class="{ active: isMenuOpen }"
-            @click="toggleMenu"
-          >
-            <svg class="plus-icon" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
-            </svg>
-
-            <!-- Dropdown Menu -->
-            <div class="dropdown-menu-mid" :class="{ active: isMenuOpen }">
-              <div
-                class="menu-item"
-                v-for="(item, index) in menuItems"
-                :key="index"
-                @click.stop="handleMenuItemClick(item)"
-              >
-                <i :class="item.icon"></i>
-                <span>{{ item.label }}</span>
-              </div>
             </div>
           </div>
         </div>
@@ -93,6 +99,7 @@
             >
               <i class="bi bi-people"></i> Общий чат
             </button>
+
             <!-- Example chat buttons -->
             <button
               v-for="n in 10"
@@ -108,7 +115,6 @@
         <!-- Bottom panel with action buttons -->
         <div class="bottom-area">
           <button class="bi bi-gear"></button>
-          <button class="bi bi-person-circle"></button>
           <button
             class="theme-toggle"
             @click="toggleTheme"
@@ -127,6 +133,7 @@
               <path :d="isDarkTheme ? moonIcon : sunIcon" />
             </svg>
           </button>
+          <button class="bi bi-person-circle"></button>
         </div>
       </div>
 
@@ -286,7 +293,7 @@ export default {
       activeCategory: 0,
       categories: ["Все", "Личные", "Группы", "Архив", "Избранное"],
       // Define thresholds
-      SNAP_THRESHOLD: 240,
+      SNAP_THRESHOLD: 260,
       MIN_THRESHOLD: 120,
     };
   },
